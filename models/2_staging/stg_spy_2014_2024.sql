ed974a4d5d87b785f72393bf2bc6fcc4721b988f
ahmed <ahmed@ahmeds-MacBook-Pro.local>
2024-08-31 21:16:45 -0400

    added daily price movement

--- models/2_staging/stg_spy_2014_2024.sql
+++ models/2_staging/stg_spy_2014_2024.sql
@@ -13,7 +13,7 @@ spy_2014_2024 as ( select * from {{ ref('base_spy_2014_2024') }} )
     left join calendar using (date)
 )
 
-, weekly_stats as 
+, weekly_price as 
 (
     select 
         year_full
@@ -23,9 +23,65 @@ spy_2014_2024 as ( select * from {{ ref('base_spy_2014_2024') }} )
     group by 1,2
 )
 
+, monthly_price as 
+(
+    select 
+        year_full
+        , month_number
+        , round(avg(avg_daily_price),2) as avg_monthly_price
+    from base
+    group by 1,2
+)
+
+, first_avg_daily_price_per_week as 
+(
+    select 
+        year_full
+        , week_number
+        , avg_daily_price as avg_daily_price_week_start
+    from base
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY year_of_date, week_number ORDER BY date asc) = 1
+)
+
+, last_avg_daily_price_per_week as 
+(
+    select 
+        year_full
+        , week_number
+        , avg_daily_price as avg_daily_price_week_end
+    from base
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY year_of_date, week_number ORDER BY date desc) = 1
+)
+
+, daily_price_movement_by_week as 
+(
+    select
+        year_full
+        , week_number
+        , ifnull(avg_daily_price_week_end,0) - ifnull(avg_daily_price_week_start,0) as daily_price_movement
+    from base
+    left join first_avg_daily_price_per_week using (year_full,week_number)
+    left join last_avg_daily_price_per_week using (year_full,week_number)
+)
+
+
+, daily_price_movement_in_week as 
+(
+    select
+        year_full
+        , week_number
+        , case 
+            when daily_price_movement < 0 then round(daily_price_movement*-1.00,2)
+            else daily_price_movement
+          end as avg_daily_price_movement_in_week
+    from daily_price_movement_by_week
+)
+
 
 select
     *
 from base
-left join weekly_stats using (year_full,week_number)
+left join weekly_price using (year_full,week_number)
+left join monthly_price using (year_full,month_number)
+left join daily_price_movement_in_week using (year_full,week_number)
 
