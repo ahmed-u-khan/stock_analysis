ca75ea9dac618ddd8be3e246af1a8fd0e7eb68da
ahmed <ahmed@ahmeds-MacBook-Pro.local>
2024-09-17 09:23:55 -0400

    added etl scripts folder with relevant scripts

---
+++ ETL Scripts/all_nasdaq_stock_info.py
@@ -0,0 +1,54 @@
+###############################################################################
+###############################################################################
+###############################################################################
+
+import datetime
+import pandas as pd
+import yfinance as yf
+import yahoo_fin.stock_info as si
+from datetime import date
+
+
+
+
+
+concat_df = pd.DataFrame()
+
+def YfinanceData(Symbols):
+
+	print('function started')
+	
+	global concat_df
+
+	for Symbol in Symbols:
+
+		print('for loop started')
+		print(Symbol)
+
+		try:
+
+			Yfinance_DataFrame = pd.DataFrame.from_dict(yf.Ticker(Symbol).info)		
+			print('Yfinance_DataFrame line started')
+
+			concat_df = pd.concat([concat_df, Yfinance_DataFrame])
+			print('concat line started')
+
+		except: 
+
+			print('unable to get stock info for'+Symbol)
+
+
+
+
+
+nasdaq = si.tickers_nasdaq()
+YfinanceData(nasdaq)
+
+concat_df = concat_df.reset_index()
+concat_df['ETL_Date'] = date.today()
+
+concat_df.to_csv('all_nasdaq_stock_info.csv')
+
+###############################################################################
+###############################################################################
+###############################################################################
