ffe9fce7f1308b24aecbdc76b8039cea046659fc
ahmed <ahmed@ahmeds-MacBook-Pro.local>
2024-09-23 19:38:32 -0400

    refactored code

--- models/3_intermediary/int_all_stock_history.sql
+++ models/3_intermediary/int_all_stock_history.sql
@@ -1,19 +1,19 @@
-with 
+-- with 
 
-all_stock_history_weekly_monthly_yearly_price as ( select * from {{ ref('stg_all_stock_history_weekly_monthly_yearly_price') }} )
+-- all_stock_history_weekly_monthly_yearly_price as ( select * from {{ ref('stg_all_stock_history_weekly_monthly_yearly_price') }} )
 
-, all_stock_weekly_price_movement as ( select * from {{ ref('stg_all_stock_weekly_price_movement') }} )
+-- , all_stock_weekly_price_movement as ( select * from {{ ref('stg_all_stock_weekly_price_movement') }} )
 
-, all_stock_monthly_price_movement as ( select * from {{ ref('stg_all_stock_monthly_price_movement') }} )
+-- , all_stock_monthly_price_movement as ( select * from {{ ref('stg_all_stock_monthly_price_movement') }} )
 
-, all_stock_yearly_price_movement as ( select * from {{ ref('stg_all_stock_yearly_price_movement') }} )
+-- , all_stock_yearly_price_movement as ( select * from {{ ref('stg_all_stock_yearly_price_movement') }} )
 
 
 
-select
-    distinct *
-from all_stock_history_weekly_monthly_yearly_price
-left join all_stock_weekly_price_movement using (year_full, week_number, symbol)
-left join all_stock_monthly_price_movement using (year_full, month_number, symbol)
-left join all_stock_yearly_price_movement using (year_full, symbol)
+-- select
+--     distinct *
+-- from all_stock_history_weekly_monthly_yearly_price
+-- left join all_stock_weekly_price_movement using (year_full, week_number, symbol)
+-- left join all_stock_monthly_price_movement using (year_full, month_number, symbol)
+-- left join all_stock_yearly_price_movement using (year_full, symbol)
 
--- models/4_final/stock_analysis.sql
+++ models/4_final/stock_analysis.sql
@@ -1,7 +1,36 @@
-with
+-- with
 
-stock_analysis as ( select * except (ETL_Date) from {{ ref('int_all_stock_history') }} )
+-- stock_analysis as ( select * except (ETL_Date) from {{ ref('int_all_stock_history') }} )
+
+-- select
+--     *
+-- from stock_analysis
+
+
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- -- -- -- -- from int_all_stock_history -- -- -- -- --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+
+with 
+
+all_stock_history_weekly_monthly_yearly_price as ( select * from {{ ref('stg_all_stock_history_weekly_monthly_yearly_price') }} )
+
+, all_stock_weekly_price_movement as ( select * from {{ ref('stg_all_stock_weekly_price_movement') }} )
+
+, all_stock_monthly_price_movement as ( select * from {{ ref('stg_all_stock_monthly_price_movement') }} )
+
+, all_stock_yearly_price_movement as ( select * from {{ ref('stg_all_stock_yearly_price_movement') }} )
+
+
 
 select
-    *
-from stock_analysis
+    distinct *
+from all_stock_history_weekly_monthly_yearly_price
+left join all_stock_weekly_price_movement using (year_full, week_number, symbol)
+left join all_stock_monthly_price_movement using (year_full, month_number, symbol)
+left join all_stock_yearly_price_movement using (year_full, symbol)
+
