b49af701654ff2602e6ca4898a501352e786ec05
ahmed <ahmed@ahmeds-MacBook-Pro.local>
2025-04-22 08:22:27 -0400

    re-org and new logic

--- models/2_staging/stg_polygon_stock_histories_1m.sql
+++ models/2_staging/stg_polygon_stock_histories_1m.sql
@@ -16,6 +16,12 @@ stock as ( select * from {{ ref('base_polygon_stock_histories_1m') }} )
         , case when cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) >= 9.30 and cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) < 16.0 then 1 else 0 end as minute_within_regular_trading_hours
         , case when cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) < 9.30 then 1 else 0 end as minute_within_pre_market_trading_hours
         , case when cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) >= 16.0 then 1 else 0 end as minute_within_post_market_trading_hours
+        , case
+            when cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) >= 9.30 and cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) < 16.0 then 'minute_within_regular_trading_hours'
+            when cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) < 9.30 then 'minute_within_pre_market_trading_hours'
+            when cast( concat(hour,'.',trim( CAST( minute AS STRING FORMAT '00') )) as numeric ) >= 16.0 then 'minute_within_post_market_trading_hours'
+            else NULL
+        end as minute_in_pre_market_regular_market_post_market_hours
     from stock
     left join calendar using (date)
 )
